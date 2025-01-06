cd11b7defe0b27fd2a232bd92a88f99b260fdf27
Ivan <burdolevich@gmail.com>
2025-01-06 20:50:07 +0300

    init: add first tasks

--- src/stack.js
+++ src/stack.js
@@ -13,22 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor() {
+        this.element = [];
+    }
+    push(item) {
+        this.element.push(item);
+    }
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    pop() {
+        return this.element.pop();
+    }
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    peek() {
+        return this.element[this.element.length - 1];
+    }
 }
 
 module.exports = {
-  Stack
+    Stack,
 };
